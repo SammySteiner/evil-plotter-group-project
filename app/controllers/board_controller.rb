8cda8720aced958f33098ce2c96b5de67daadee9
drumnation <davidmieloch@gmail.com>
2017-05-09 12:01:42 -0400

    finished board controller

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,27 +1,47 @@
 class BoardController < ApplicationController
+  before_action: set_board, only: [:show, :update, :delete]
 
   def index
-
+    # to "users/:id/boards" because of this filter by user
+    @boards = Board.all.where("user_id = ?", user.id)
+    render json: @boards
   end
 
-  def new
-
+  def show
+    render json: @board
   end
 
   def create
-
-  end
-
-  def edit
+		@board = Board.new(board_params)
+		if @board.save
+			render json: @board
+		else
+			puts "create error"
+		end
 
   end
 
   def update
-
+    if @board.update(board_params)
+			render json: @board
+    else
+      puts "update error"
+		end
   end
 
   def delete
+    @board.destroy
+    puts "board deleted"
+  end
 
+  private
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:title)
   end
 
 end
