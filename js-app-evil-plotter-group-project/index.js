931e874b9f8263eed0d783334f378e4a99f7932c
drumnation <davidmieloch@gmail.com>
2017-05-10 09:38:08 -0400

    add multiple notes

--- js-app-evil-plotter-group-project/index.js
+++ js-app-evil-plotter-group-project/index.js
@@ -1,24 +1,50 @@
 $(function() {
 
-  $.ajax ({
-    url: `http://localhost:3000/api/v1/users/1/boards/1/notes/1`
-  }).then(function(note) {
-    $('div#noteContainer').html(`
-      <div class="row">
-          <div id="postIt">
-          <div class=topBar id="top"></div>
-          <div class=form>
-            <div contenteditable class=textAria id="note"></div>
+  // $.ajax ({ // stubbed out generate card from ajax request to API site
+  //   url: `http://localhost:3000/api/v1/users/1/boards/1/notes/1`
+  // }).then(function(note) {
+  //   $('div#noteContainer').append(`
+  //     <div class="row">
+  //         <div id="postIt">
+  //         <div class=topBar >
+  //           <h5 class="center-align vertical-align">${note[0].title.toUpperCase()}</h5></div>
+  //         <div class=form>
+  //           <div contenteditable class=textAria id="note">${note[0].content}</div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   `)
+
+
+  $.ajax ({ // stubbed out generate card from ajax request to API site
+    url: `http://localhost:3000/api/v1/users/1/boards/1/notes/`
+  }).then(function(notes) {
+    notes.forEach(function(note) {
+      $('div#noteContainer').append(
+        `<div class="row">
+            <div id="postIt">
+            <div class=topBar >
+              <h5 class="center-align vertical-align">${note.title.toUpperCase()}</h5></div>
+            <div class=form>
+              <div contenteditable class=textAria id="note">${note.content}</div>
+            </div>
           </div>
-        </div>
-      </div>
-    `)
-    $(function(){
-      $("#postIt").draggable({
-        handle: '.topBar',
+        </div>`
+      ).done(function(note) {
+        $("#mydiv").css({top: note.y, left: note.x, position:'absolute'})
+        // change DB to top and left
+        // find out x and y range
+        // add positions to seed file
       })
+
+      $(function(){
+        $("#postIt").draggable({
+          handle: '.topBar',
+        })
+      })
+
     })
-    $('.topBar').html(`<h5 class="center-align vertical-align">${note[0].title.toUpperCase()}</h5>`)
-    $('div#note').html(`${note[0].content}`)
   })
 })
+
+style="position: relative; left: 290px; top: 119px;"
