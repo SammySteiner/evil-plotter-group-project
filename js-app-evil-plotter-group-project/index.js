54a74a1cf1a11122d2410110e51c570fc7cd3932
drumnation <davidmieloch@gmail.com>
2017-05-10 00:21:17 -0400

    stubbed out generate card from ajax request to API site

--- js-app-evil-plotter-group-project/index.js
+++ js-app-evil-plotter-group-project/index.js
@@ -1,14 +1,24 @@
 $(function() {
-  $.ajax({
-    url: 'http://localhost:3000/api/v1/boards'
-  }).then(function(board) {
-    $('').text(note)
-  })
 
-  $.post({
-    url: 'http://localhost:3000/api/v1/users'
-  }).then(function(user) {
-    
+  $.ajax ({
+    url: `http://localhost:3000/api/v1/users/1/boards/1/notes/1`
+  }).then(function(note) {
+    $('div#noteContainer').html(`
+      <div class="row">
+          <div id="postIt">
+          <div class=topBar id="top"></div>
+          <div class=form>
+            <div contenteditable class=textAria id="note"></div>
+          </div>
+        </div>
+      </div>
+    `)
+    $(function(){
+      $("#postIt").draggable({
+        handle: '.topBar',
+      })
+    })
+    $('.topBar').html(`<h5 class="center-align vertical-align">${note[0].title.toUpperCase()}</h5>`)
+    $('div#note').html(`${note[0].content}`)
   })
-
 })
