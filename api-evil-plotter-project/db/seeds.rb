df77608149d7b8ae92359f9b4d16541bf41c1117
markdegroat <mhdegroat@gmail.com>
2017-05-10 14:50:11 -0400

    Committing before master merge

--- api-evil-plotter-project/db/seeds.rb
+++ api-evil-plotter-project/db/seeds.rb
@@ -12,8 +12,8 @@ require 'faker'
   name = Faker::Name.name
   new_user = User.new(name: name, password: "123", password_confirmation: "123")
   new_user.save
-  puts new_user.errors.messages
-  puts new_user
+  # puts new_user.errors.messages
+  # puts new_user
 end
 
 300.times do
