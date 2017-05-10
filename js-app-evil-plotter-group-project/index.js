b7f6b4ccd097de37e15980124d91c9b6795600dd
markdegroat <mhdegroat@gmail.com>
2017-05-10 14:53:34 -0400

    committing changes before merging from master

--- js-app-evil-plotter-group-project/index.js
+++ js-app-evil-plotter-group-project/index.js
@@ -1,14 +1,15 @@
 $(function() {
-  $.ajax({
-    url: 'http://localhost:3000/api/v1/boards'
-  }).then(function(board) {
-    $('').text(note)
-  })
+  // $.ajax({
+  //   url: 'http://localhost:3000/api/v1/boards'
+  // }).then(function(board) {
+  //   $('').text(note)
+  // })
 
-  $.post({
-    url: 'http://localhost:3000/api/v1/users'
+  $.ajax({
+    url: 'http://localhost:3000/api/v1/users/1/boards',
+    method: 'get'
   }).then(function(user) {
-    
+    console.log(user)
   })
 
 })
