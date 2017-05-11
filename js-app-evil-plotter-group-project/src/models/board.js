84808099af640ab606b0ce183af632970ba03277
drumnation <davidmieloch@gmail.com>
2017-05-11 12:59:37 -0400

    tweaks

--- js-app-evil-plotter-group-project/src/models/board.js
+++ js-app-evil-plotter-group-project/src/models/board.js
@@ -4,6 +4,10 @@ class Board {
     Board.all.push(this) // push instance of self into array of board objects
   }
 
+  showNotes(){
+    console.log(this.notes)
+  }
+
   addNote(title, content, left, top, height, width) {
     this.notes.push(new Note(title, content, left, top, height, width))
   }
