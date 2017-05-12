a1f0a9d7073330e5765a7f353d65386c6024d0a4
drumnation <davidmieloch@gmail.com>
2017-05-12 10:57:48 -0400

    minor tweaks

--- js-app-evil-plotter-group-project/src/views/boardView.js
+++ js-app-evil-plotter-group-project/src/views/boardView.js
@@ -2,20 +2,24 @@ class BoardView{
   renderBoards(boards) {
     var inner_html = ''
     boards.forEach( function(board){
-      inner_html += `<div class="row">
-        <div class="col s12 m6">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="board card-title" id="board-${board.id}">${board.title}</span>
-              <p>Well, that's the trick, isn't it? And it's going to cost you something extra. Ten thousand in advance. Ten thousand?</p>
-            </div>
-            <div class="card-action">
-              <a href="#" class="board" id="board-${board.id}">View</span>
-              <a href="#" class="delete-board" id="delete-board-${board.id}">Delete</a>
+      inner_html += `
+        <div class="col s6"></div>
+          <div class="col s6">
+            <div class="col s6">
+              <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                  <span class="board card-title" id="board-${board.id}">${board.title}</span>
+                  <p>Well, that's the trick, isn't it? And it's going to cost you something extra. Ten thousand in advance. Ten thousand?</p>
+                </div>
+                <div class="card-action">
+                  <span class="board" id="board-${board.id}"><a>View</a></span>
+                  <a href="#" class="delete-board" id="delete-board-${board.id}">Delete</a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>`
+
+    <div class="col s3"></div>`
     })
     return `<div class='row'>${inner_html}</div>`
   }
@@ -27,12 +31,11 @@ class BoardView{
   }
 
   BoardName(title) {
-    return `<h1>${title}</h1>`
+    return `<h1 class="center-align">${title}!!!</h1>`
   }
 
 }
 
-
-      // <div class="col s5">
-      // <span class="board blue-text text-darken-2" id = "board-${board.id}" >${board.title}</span>
-      // </div><a href="#" class="delete-board col s1" id="delete-board-${board.id}">delete</a>
+// <div class="col s5">
+// <span class="board blue-text text-darken-2" id = "board-${board.id}" >${board.title}</span>
+// </div><a href="#" class="delete-board col s1" id="delete-board-${board.id}">delete</a>
--- js-app-evil-plotter-group-project/src/views/noteView.js
+++ js-app-evil-plotter-group-project/src/views/noteView.js
@@ -10,6 +10,4 @@ class NoteView {
         </div>
       </div>`
   }
-
-
 }
